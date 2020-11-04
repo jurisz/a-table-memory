3a8b4833880c4ac8206dd0d3bc745c1694b6a521
juris <juris.zubkans@gmail.com>
2020-11-04 23:23:16 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<!--             :row-selection="{selectedRowKeys: selectedDevices, onChange: onDeviceSelectChange}"-->
+    <a-table :columns="columnDefinitions"
+             :data-source="items"
+             :pagination="false"
+             @change="tableChange"
+             rowKey="id">
+    </a-table>
+
+    <a-pagination
+        background
+        layout="prev, pager, next"
+        :current="page"
+        :pageSize="50"
+        :total="250"
+        size="default"
+        @change="pageChange">
+    </a-pagination>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+const columnDefinitions = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    sorter: true,
+  },
+  {
+    title: 'Building',
+    dataIndex: 'buildingAddress',
+    sorter: true,
+  },
+  {
+    title: 'RoomGroup',
+    dataIndex: 'roomGroupName',
+    sorter: true
+  },
+  {
+    title: 'Info',
+    dataIndex: 'info',
+    sorter: true,
+  },
+  {
+    title: 'Device',
+    dataIndex: 'deviceName',
+    sorter: true,
+  },
+  {
+    title: 'Gateway',
+    dataIndex: 'gwName',
+    sorter: true,
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    sorter: true,
+  },
+  {
+    title: 'Status',
+    dataIndex: 'online',
+    sorter: true,
+  }
+]
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      columnDefinitions,
+      items: [],
+      page: 1,
+      selectedDevices: [],
+    }
+  },
+  async created() {
+    await this.loadPage(1)
+  },
+  methods: {
+    async loadPage(nr) {
+      const resp = await this.$http.get(`/assets/page${nr}.json`)
+      this.items = resp.data.results
+    },
+    pageChange(page) {
+      this.page = page;
+      this.loadPage(page);
+    },
+    tableChange(pagination, filter, sorter) {
+      this.loadPage(this.page);
+    },
+    onDeviceSelectChange(selectedRowKeys) {
+      this.selectedDevices = selectedRowKeys;
+    },
   }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import Axios from 'axios';
+
+Vue.prototype.$http = Axios;
+
 Vue.config.productionTip = false
+Vue.use(Antd);
 
 new Vue({
   render: h => h(App),
